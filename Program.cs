0e7256e768aa2504434886a6c4bf521e9a259b3a
Ramon Santos <ramonss.bque@gmail.com>
2024-11-12 23:19:34 -0300

    Criado lógica para limite e desempenho de listas

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Diagnostics;
+using System.Collections;
+const int NUM_ITENS = 1000000;
+
+const int INITIAL_CAPACITY = 100000;
+
+var stopwatch = new Stopwatch();
+
+stopwatch.Start();
+
+// Lista com tamanho inicial definido
+
+var lista1 = new List<int>(INITIAL_CAPACITY);
+
+for (int i = 0; i < NUM_ITENS; i++)
+{
+   lista1.Add(i);
+}
+
+stopwatch.Stop();
+
+Console.WriteLine($"Tempo para adicionar " + NUM_ITENS + "itens a uma lista com tamanho inicial de " + INITIAL_CAPACITY + ": {stopwatch.ElapsedMilliseconds} ms");
+
+stopwatch.Reset();
+stopwatch.Start();
+
+var lista2 = new List<int>();
+
+for (int i = 0; i < NUM_ITENS; i++)
+{
+   lista2.Add(i);
+}
+
+stopwatch.Stop();
+
+Console.WriteLine("Tempo para adicionar " + NUM_ITENS + " itens a uma lista sem tamanho inicial definido: " + stopwatch.ElapsedMilliseconds + " ms");
+
+Console.ReadKey();
